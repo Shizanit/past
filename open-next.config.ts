37c3277a709f25aea88b79c3dcde3b848548c947
Shizanit <65107731+Shizanit@users.noreply.github.com>
2026-05-12 21:43:22 +0000

    test

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,4 @@
-import type { OpenNextConfig } from "@opennextjs/cloudflare";
-
-const config: OpenNextConfig = {
+export default {
   default: {
     override: {
       wrapper: "cloudflare-node",
@@ -18,5 +16,3 @@ const config: OpenNextConfig = {
     },
   },
 };
-
-export default config;

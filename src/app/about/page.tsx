6bf52fbe2547d314b76f85d7b55d056e4903be3b
Shizanit <65107731+Shizanit@users.noreply.github.com>
2026-05-12 22:15:33 +0000

    test

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,10 @@
 import { EditorView } from "@/components/editor/editor-view";
 import { env } from "@/lib/env";
-import * as fs from "node:fs/promises";
-import * as path from "node:path";
 import { Suspense } from "react";
+import readmeRaw from "@/../../README.md";
 
-export const dynamic = "force-static"; // ← добавить это
-
-export default async function AboutPage() {
-  const readmePath = path.join(process.cwd(), "README.md");
-
-  let readme = await fs.readFile(readmePath, "utf-8");
+export default function AboutPage() {
+  let readme = readmeRaw as string;
   readme = readme.replace(
     '<a href="https://vercel.com/new/project?template=jozsefsallai/fastbin-rewritten"><img width="128" src="https://vercel.com/button" alt="One-click Deployment" /></a>',
     "https://vercel.com/new/project?template=jozsefsallai/fastbin-rewritten",
